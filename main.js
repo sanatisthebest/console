0a9ff26a2c1e57b49f23cd6e5e0b45c85447e9f2
sanatisthebest <129635227+sanatisthebest@users.noreply.github.com>
2023-09-10 11:50:05 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var a = 10;
+var b = 15;
+
+var first_name ="canon";
+var last_name ="shore";
+
+var add = a+b;
+console.log(add);
+var full_name = first_name.concat(last_name);
+console.log(full_name);
